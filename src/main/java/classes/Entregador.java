73d3749f5eca10eb26a8be8dd56028f18c59f413
AdrianoSilva22 <adrianodesilva22@gmail.com>
2023-12-14 20:47:28 -0300

    Alterando Entregador.

--- src/main/java/classes/Entregador.java
+++ src/main/java/classes/Entregador.java
@@ -3,7 +3,7 @@ import javax.persistence.Entity;
 @Entity
 public class Entregador extends Funcionario {
 
-    public Entregador(int id, String nome, String endereco) {
+    public Entregador(int id, String nome) {
         super(id, nome);
     }
 
